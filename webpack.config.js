ca1895f7bffb99ceef3870c3f7c95c9c174afdeb
sergey <sergey@WitcherII>
2017-07-12 23:11:38 +0300

    code format

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,33 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  entry: {
-    server: './src/server.ts'
+  entry : {
+    server : './src/server.ts'
   },
-  resolve: {
-    extensions: ['.ts', '.js']
+  resolve : {
+    extensions : [ '.ts', '.js' ]
   },
-  target: 'node',
-   externals: [nodeExternals({
-     whitelist: [
-       /^@angular\/material/,
-       /^@ngx-translate\/core/,
-       /^@ngx-translate\/http-loader/,
-     ]
-   })],
-  node: {
-    __dirname: true
+  target : 'node',
+  externals : [ nodeExternals({
+    whitelist : [
+      /^@angular\/material/,
+      /^@ngx-translate\/core/,
+      /^@ngx-translate\/http-loader/,
+    ]
+  }) ],
+  node : {
+    __dirname : true
   },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].js'
+  output : {
+    path : path.join(__dirname, 'dist'),
+    filename : '[name].js'
   },
-  module: {
-    rules: [
-      { test: /\.ts$/, loader: 'ts-loader' }
+  module : {
+    rules : [
+      {
+        test : /\.ts$/,
+        loader : 'ts-loader'
+      }
     ]
   }
 }
